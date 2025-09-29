b46d31863ce42d46f513bd70b0db52da426c7463
nadia-nh <nadia.heredia.nh@gmail.com>
2025-09-29 14:38:51 -0400

    Update tomato and corn crops with actual textures and prices

--- Scripts/crop_data.gd
+++ Scripts/crop_data.gd
@@ -2,6 +2,6 @@ class_name CropData
 extends Resource
 
 @export var growth_sprites: Array[Texture]
-@export var days_to_grow: int = 8
+@export var days_to_grow: int = 5
 @export var seed_price: int = 10
 @export var sell_price: int = 20
