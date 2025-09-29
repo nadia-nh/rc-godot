43f0d7873d9e7874d3c9c375c9a9d0dbfabd72ae
nadia-nh <nadia.heredia.nh@gmail.com>
2025-09-29 14:32:11 -0400

    Add CropData script
    This script represents data, and is not attached to a particular node, it will be used to represent the crops that can be planted and harvested.

---
+++ Scripts/crop_data.gd
@@ -0,0 +1,7 @@
+class_name CropData
+extends Resource
+
+@export var growth_sprites: Array[Texture]
+@export var days_to_grow: int = 8
+@export var seed_price: int = 10
+@export var sell_price: int = 20
