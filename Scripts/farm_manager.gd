f0e5d1b0951297b18943fb86e05a2003d062b1bf
nadia-nh <nadia.heredia.nh@gmail.com>
2025-10-01 19:30:23 -0400

    Add initial functions to the FarmManager's script
    Initialize the used cells to tile info map, and allow setting a tile's state.

--- Scripts/farm_manager.gd
+++ Scripts/farm_manager.gd
@@ -16,8 +16,32 @@ class TileInfo:
 
 var crop_scene: PackedScene = preload("res://Scenes/crop.tscn")
 var used_cells_to_tile_info: Dictionary[Vector2i, TileInfo]
-var tyle_type_to_tile_set_coordinates: Dictionary[TileType, Vector2i] = {
+var tile_type_to_tile_set_coords: Dictionary[TileType, Vector2i] = {
     TileType.UNTILLED_GRASS: Vector2i(0, 0),
     TileType.TILLED_GRASS: Vector2i(1, 0),
     TileType.TILLED_AND_WATERED_GRASS: Vector2i(0, 1),
 }
+
+func _ready():
+    # TODO: Connect to signals
+    
+    # Initialize used_cells_to_tile_info map
+    for cell in tile_map.get_used_cells():
+        used_cells_to_tile_info[cell] = TileInfo.new()
+
+func set_tile_state(coords: Vector2i, tile_type: TileType):
+    # Update the sprite
+    # Source_id = 0 since we're using the first and only tile_set / atlas
+    tile_map.set_cell(coords, 0, tile_type_to_tile_set_coords[tile_type])
+
+    # Update the tile info
+    match tile_type:
+        TileType.UNTILLED_GRASS:
+            used_cells_to_tile_info[coords].tilled = false
+            used_cells_to_tile_info[coords].watered = false
+        TileType.TILLED_GRASS:
+            used_cells_to_tile_info[coords].tilled = true
+            used_cells_to_tile_info[coords].watered = false
+        TileType.TILLED_AND_WATERED_GRASS:
+            used_cells_to_tile_info[coords].tilled = true
+            used_cells_to_tile_info[coords].tilled = true
